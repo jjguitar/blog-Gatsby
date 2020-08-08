215ccd463609b8078c0d63b2881a1db485e3d24d
Jhoncito el pillo <jhon.electronica@gmail.com>
2020-08-08 14:12:46 -0500

    Finish Project tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,73 @@
 import React from "react"
-import { graphql } from "gatsby" // highlight-line
+import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby" // highlight-line
+import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
 export default function Home({ data }) {
   return (
     <Layout>
-      <h1>Amazing Pandas Eating Things {data.site.siteMetadata.title}</h1>
       <div>
-        <img
-          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-          alt="Group of pandas eating bamboo"
-        />
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            {/* highlight-start */}
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              {/* highlight-end */}
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}
+              >
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #555;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link> {/* highlight-line */}
+          </div>
+        ))}
       </div>
     </Layout>
   )
 }
 
-// highlight-start
 export const query = graphql`
   query {
-    site {
-      siteMetadata {
-        title
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
       }
     }
   }
 `
-// highlight-end
---
+++ src/templates/blog-post.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql } from "gatsby" // highlight-line
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+// highlight-start
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark
+  // highlight-end
+  return (
+    <Layout>
+      {/* highlight-start */}
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+      {/* highlight-end */}
+    </Layout>
+  )
+}
+
+// highlight-start
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+      excerpt
+    }
+  }
+`
+// highlight-end
